e883200777514e7f2386e1632044ee0e26e8b955
Harry.Breslin <Harry.Breslin@dominic.tas.edu.au>
2020-08-14 11:26:31 +1000

    Hide button

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 var longcatShowButton = document.getElementById('longcatShowButton');
+var longcatShowDiv = document.getElementById('longcatShowDiv');
 var longcatSection = document.getElementById('longcatSection');
 
 function clickLongcatShowButton() {
 	longcatSection.classList.remove('hidden');
-	longcatShowButton.classList.add('hidden');
+	longcatShowDiv.classList.add('hidden');
 }
 
 longcatShowButton.onclick = clickLongcatShowButton;
+longcatShowDiv.onclick = clickLongcatShowButton;
